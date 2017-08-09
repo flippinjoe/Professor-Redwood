0b2e36715b1177fa8622025459b93cb434462143
Joe Ridenour <jridenour@mercury.io>
2017-08-09 10:46:50 -0500

    Don’t crash when there’s an error.

--- backend/src/bot.js
+++ backend/src/bot.js
@@ -3,8 +3,8 @@
 const Discord = require('discord.js');
 // const mySQL = require('mysql');
 
-const chatCommandsFunc = require('./src/chatrouter');
-const CONSTANTS = require('./src/constants');
+const chatCommandsFunc = require('./lib/chatrouter');
+const CONSTANTS = require('./lib/constants');
 const config = require('./config/secrets.json');
 
 const client = new Discord.Client();
@@ -59,54 +59,63 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-	if ((message.member && message.member.id === CONSTANTS.BOTID) ||
-		(message.author && message.author.id === CONSTANTS.BOTID)) return;
 
-	//TESTING PURPOSES ONLY PLEASE REMOVE
-	//if (message.channel.name !== 'professor_redwood') return;
+	try {
+		if ((message.member && message.member.id === CONSTANTS.BOTID) ||
+			(message.author && message.author.id === CONSTANTS.BOTID)) return;
 
-	if (message.channel.type === 'dm' || message.channel.type === 'group') {
-		message.channel.send('I currently have no direct message functions. Please go to channel #start_here');
-		return;
-	}
+		//TESTING PURPOSES ONLY PLEASE REMOVE
+		//if (message.channel.name !== 'professor_redwood') return;
 
-	if (message.content[0] !== '!') {
-		if (message.member)	{CHATCOMMANDS.checkNew(message);}
-		return;
-	}
-
-	let reply = '';
-	const command = message.content.split(' ')[0];
-
-	//Outside of Professor Redwood Channel, Message.member has NOT been null checked yet
-	if (command === '!raid') {
-		if (message.channel.name.indexOf('-') === -1) {
-			reply = message.member.displayName + ', raid commands should only be run in the corresponding neighborhood channel';
-			message.channel.send(reply);
-			return reply;
+		if (message.channel.type === 'dm' || message.channel.type === 'group') {
+			message.channel.send('I currently have no direct message functions. Please go to channel #start_here');
+			return;
 		}
-		CHATCOMMANDS.raid(message);
-	}
-	//Inside Professor Redwood Channel, Do not touch message.member
-	else if (message.channel.name !== 'professor_redwood') {
-		message.channel.send(message.member.displayName + ', you may only run this command in the ' + channelsByName['professor_redwood'] + ' channel');
-		return;
-	}
-   
-	if (command === '!breakpoint' || command === '!bp') {return CHATCOMMANDS.breakpoint(message);}
-	else if (command === '!cp') {return CHATCOMMANDS.cp(message);}
-	else if (command === '!counter' || command === '!counters') {CHATCOMMANDS.counters(message);}
-	else if (command === '!help') {CHATCOMMANDS.help(message);}
 
-	//Inside Professor Redwood Channel, OK to touch message.member
-	if (reply === '' && !message.member) {
-		message.channel.send('Member is a ' + getEmoji('gengar') + ' - Commands cannot be run for users who are invisible, please remove your invisible status');
-		return;
+		if (message.content[0] !== '!') {
+			if (message.member)	{CHATCOMMANDS.checkNew(message);}
+			return;
+		}
+
+		let reply = '';
+		const command = message.content.split(' ')[0];
+
+		//Outside of Professor Redwood Channel, Message.member has NOT been null checked yet
+		if (command === '!raid') {
+			if (message.channel.name.indexOf('-') === -1) {
+				reply = message.member.displayName + ', raid commands should only be run in the corresponding neighborhood channel';
+				message.channel.send(reply);
+				return reply;
+			}
+			CHATCOMMANDS.raid(message);
+		}
+		//Inside Professor Redwood Channel, Do not touch message.member
+		else if (message.channel.name !== 'professor_redwood') {
+			message.channel.send(message.member.displayName + ', you may only run this command in the ' + channelsByName['professor_redwood'] + ' channel');
+			return;
+		}
+
+		if (command === '!breakpoint' || command === '!bp') {return CHATCOMMANDS.breakpoint(message);}
+		else if (command === '!cp') {return CHATCOMMANDS.cp(message);}
+		else if (command === '!counter' || command === '!counters') {CHATCOMMANDS.counters(message);}
+		else if (command === '!help') {CHATCOMMANDS.help(message);}
+
+		//Inside Professor Redwood Channel, OK to touch message.member
+		if (reply === '' && !message.member) {
+			message.channel.send('Member is a ' + getEmoji('gengar') + ' - Commands cannot be run for users who are invisible, please remove your invisible status');
+			return;
+		}
+
+		if (command === '!play') {CHATCOMMANDS.play(message);}
+		else if (command === '!team') {CHATCOMMANDS.team(message);}
+		else if (command === '!want') {CHATCOMMANDS.want(message);}
+	}
+	catch (ex) {
+		message.channel.send(`Sorry ${message.member.displayName}.  Looks like I've broken down on what you wanted. Here's an error to help @tech.  "${ex.message}"`);
+		console.error('Testing ', ex);
 	}
 
-	if (command === '!play') {CHATCOMMANDS.play(message);}
-	else if (command === '!team') {CHATCOMMANDS.team(message);}
-	else if (command === '!want') {CHATCOMMANDS.want(message);}
+
 
 });
 
